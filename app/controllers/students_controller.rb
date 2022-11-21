7e3a5b78ee66584c02737a708ce3d5a68e77bf8b
Nick <nfears5798@gmail.com>
2022-11-21 17:35:57 -0500

    commit tree 720ae540da53f22da53a6ae3a04029edb677cb27

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+                #  Student.by_name(params[:name])
+               #return student with the same name as (matching first or last name)
+               Student.where('lower(last_name) = ? OR lower(first_name) = ?', params[:name].downcase, params[:name].downcase)
+               else
+                 Student.all
+               end    
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
